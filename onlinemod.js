092d1793e1a1a772d7a7d98e2fe9e63a46a60736
scabrum <139222113+scabrum@users.noreply.github.com>
2023-07-11 16:57:45 +0300

    Add files via upload

---
+++ onlinemod.js
@@ -0,0 +1,43 @@
+(function () {
+    'use strict';
+
+    var timer = setInterval(function(){
+        if(typeof Lampa !== 'undefined'){
+            clearInterval(timer);
+            start();
+           }
+    },10);
+
+      function start(){
+
+        var plugins = Lampa.Plugins.get();
+
+        var plugins_add = [
+            {
+                "url": "https://nb557.github.io/plugins/online_mod.js", 
+                "status": 1,
+                "name": "Online Mod",
+                "author": "@t_anton"
+            }
+        ];
+
+        var plugins_push = []
+
+        plugins_add.forEach(function(plugin){
+            if(!plugins.find(function(a){ return a.url == plugin.url})){
+                Lampa.Plugins.add(plugin);
+                Lampa.Plugins.save();
+
+                plugins_push.push(plugin.url)
+            }
+        });
+
+        if(plugins_push.length) Lampa.Utils.putScript([plugins_push],function(){},function(){},function(){},true);
+
+        /*
+        setTimeout(function(){
+            Lampa.Noty.show('Плагины установлены, перезагрузка через 5 секунд.',{time: 5000})
+        },5000)
+        */
+    }
+})();
---
+++ record_mod.js
@@ -0,0 +1,43 @@
+(function () {
+    'use strict';
+
+    var timer = setInterval(function(){
+        if(typeof Lampa !== 'undefined'){
+            clearInterval(timer);
+            start();
+           }
+    },10);
+
+      function start(){
+
+        var plugins = Lampa.Plugins.get();
+
+        var plugins_add = [
+            {
+                "url": "https://nemiroff.github.io/lampa/rr.js",
+                "status": 1,
+                "name": "Радио Record Mod",
+                "author": "@Tw1cker"
+            }
+        ];
+
+        var plugins_push = []
+
+        plugins_add.forEach(function(plugin){
+            if(!plugins.find(function(a){ return a.url == plugin.url})){
+                Lampa.Plugins.add(plugin);
+                Lampa.Plugins.save();
+
+                plugins_push.push(plugin.url)
+            }
+        });
+
+        if(plugins_push.length) Lampa.Utils.putScript([plugins_push],function(){},function(){},function(){},true);
+
+        /*
+        setTimeout(function(){
+            Lampa.Noty.show('Плагины установлены, перезагрузка через 5 секунд.',{time: 5000})
+        },5000)
+        */
+    }
+})();
