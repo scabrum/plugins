3b7075745e28340cb2909a2c12a6524e8f980b90
scabrum <139222113+scabrum@users.noreply.github.com>
2023-07-28 13:45:51 +0300

    Update addon.js

--- addon.js
+++ addon.js
@@ -140,7 +140,7 @@ Lampa.SettingsApi.addComponent({
                             },
 					onChange: function(value) {
 						if (value == '1') {
-							itemON('http://cub.watch/plugin/tmdb-proxy', 'TMDB Proxy', '@lampa', 'TMDB Proxy');
+							itemON('http://cub.watch/plugin/tmdb-proxy', 'TMDB Proxy', '@lampa', 'TMDB');
 						}
 						if (value == '2') {
 							var pluginToRemoveUrl = "http://cub.watch/plugin/tmdb-proxy";
