3d8586e3e10045affe2cca6e6200010b0c19bb57
scabrum <139222113+scabrum@users.noreply.github.com>
2023-08-13 14:48:13 +0300

    Update addon.js

--- addon.js
+++ addon.js
@@ -100,6 +100,10 @@ Lampa.SettingsApi.addComponent({
 				}, 0);
 				$("#hideInstall").remove();
 				//$('body').append('<div id="hideInstall"><style>div.settings-param__value{opacity: 0%!important;display: none;}</style><div>')
+		                /* Сдвигаем раздел выше */
+				        setTimeout(function() {
+					  $('div[data-component=plugins]').before($('div[data-component=add_plugin]'))
+					}, 30)
 			}
 		});
 
