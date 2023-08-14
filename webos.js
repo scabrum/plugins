eac5f169fa6fb6b5229603b2dd4a2bbe81235dcf
scabrum <139222113+scabrum@users.noreply.github.com>
2023-08-15 00:47:49 +0300

    Update webos.js

--- webos.js
+++ webos.js
@@ -468,12 +468,6 @@ Lampa.SettingsApi.addComponent({
 							component: 'add_online_plugin',
 							name: 'Online'
 						});
-					/* Сдвигаем раздел выше */
-						setTimeout(function() {
-							$('div[data-component=add_plugin]').remove();
-							$('div[data-component=plugins]').before('<div class="settings-folder selector" data-component="add_plugin"><div class="settings-folder__icon">' + icon_add_plugin + '</div><div class="settings-folder__name">Плагины</div></div>')
-						}, 30)
-					}
 		});
 /* Онлайн */
 		Lampa.SettingsApi.addParam({
