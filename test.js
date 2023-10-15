2f13cc9f00e5d99bb3bbc4bc24dbe592622adfff
scabrum <139222113+scabrum@users.noreply.github.com>
2023-10-15 23:55:35 +0300

    Update test.js

--- test.js
+++ test.js
@@ -29,7 +29,7 @@ Lampa.Modal.open({
         onSelect: function onSelect() {
           Lampa.Modal.close();
           $('.modal').remove();
-	  Lampa.Controller.toggle('settings');
+	  Lampa.Controller.toggle('');
         }
       }, {
         name: 'Да',
