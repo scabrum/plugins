a2e8b7e311083a2bce28b3ee6f043177ff43d540
scabrum <139222113+scabrum@users.noreply.github.com>
2024-01-21 12:53:09 +0300

    Update addon.js

--- addon.js
+++ addon.js
@@ -2,7 +2,7 @@
     'use strict';
 	var plugins = Lampa.Storage.get('plugins','[]')
 	plugins.forEach(function(plug) {
-		plug.url = (plug.url + '').replace('https://scabrum.github.io/plugins/addon.js', 'https://tinyurl.com/lampashop');
+		plug.url = (plug.url + '').replace('https://tinyurl.com/lampashop', 'https://scabrum.github.io/plugins/addon.js');
 	})	
 	Lampa.Storage.set('plugins',plugins)
 function addonStart() {
