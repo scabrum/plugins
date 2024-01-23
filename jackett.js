4ebb86ebb32fc5efd21043c9289199e441b72424
scabrum <139222113+scabrum@users.noreply.github.com>
2024-01-24 00:15:51 +0300

    Update jackett.js

--- jackett.js
+++ jackett.js
@@ -2,7 +2,7 @@
 	'use strict';
 	var plugins = Lampa.Storage.get('plugins','[]')
 	plugins.forEach(function(plug) {
-		plug.url = (plug.url + '').replace('https://scabrum.github.io/plugins/jackett.js', 'https://bylampa.github.io/jackett.js');
+		plug.url = (plug.url + '').replace('https://scabrum.github.io/plugins/jackett.js', 'https://bylampa.github.io/jackett.js').replace('http://scabrum.github.io/plugins/jackett.js', 'https://bylampa.github.io/jackett.js');
 	})	
 	Lampa.Storage.set('plugins',plugins)
 Lampa.Platform.tv();
